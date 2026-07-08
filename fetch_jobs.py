6c3b32e37afc0a2ac4abd1926f2361dfbd4a32c1
sant2900 <86867761+sant2900@users.noreply.github.com>
2026-07-08 13:28:33 +0530

    Update fetch_jobs.py

--- fetch_jobs.py
+++ fetch_jobs.py
@@ -1,8 +1,3 @@
-- name: Install Requests
-      run: pip install requests
-
-    - name: Run Job Scraper Bot
-      env:
-        TELEGRAM_BOT_TOKEN: ${{ secrets.TELEGRAM_BOT_TOKEN }}
-        TELEGRAM_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}
-      run: python fetch_jobs.py
+import requests
+import json
+# ... aapka baaki scraper code
